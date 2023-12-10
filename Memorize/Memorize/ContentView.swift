ab43d3318f47508216253375b4aa9e9016305087
zkan <zkan.cs@gmail.com>
2023-12-10 21:26:18 +0700

    Use type inference

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -26,7 +26,7 @@ struct CardView: View {
     var body: some View {
         ZStack {
             // let means a constant
-            let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+            let base = RoundedRectangle(cornerRadius: 12)
 
             if isFaceUp {
                 base.fill(.white)
