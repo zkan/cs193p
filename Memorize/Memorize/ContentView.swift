419a5806a962befd5478249547eb84b115754367
zkan <zkan.cs@gmail.com>
2023-12-18 21:48:18 +0700

    Use lazy grid, set opacity for flipping cards, and use scroll view

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -16,16 +16,20 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            cards
+            ScrollView {
+                cards
+            }
+            Spacer()
             cardCountAdjusters
         }
         .padding()
     }
 
     var cards: some View {
-        HStack {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
             ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
@@ -67,14 +71,13 @@ struct CardView: View {
         ZStack {
             // let means a constant
             let base = RoundedRectangle(cornerRadius: 12)
-
-            if isFaceUp {
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-            } else {
-                base
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }.onTapGesture {
             print("tapped")
 
